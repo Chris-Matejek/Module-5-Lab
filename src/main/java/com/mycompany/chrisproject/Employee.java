e66815dd20128d990e7c601ef51c939b9e2cee0a
@Christopher <>
2023-04-04 18:54:26 -0400

    Lab number 5

---
+++ src/main/java/com/mycompany/chrisproject/Employee.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.chrisproject;
+
+/**
+ *
+ * @author User
+ */
+public class Employee extends CommissionEmployee {
+
+    private static class hoursworked {
+
+        public hoursworked() {
+        }
+    }
+    
+    if (hoursworked <= 168.0 || hoursworked >= 0.0) static {
+         throw new IllegalArgumentException(
+            "hoursworked must be > 0.0 and < 168.0");
+    private double hoursworked;
+         this.double = hoursworked;
+    }
+}
